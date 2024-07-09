08aa4d27f112444bfc26681bb9f164663453435a
fai-sas <craftedbyfaisal@gmail.com>
2024-07-09 20:09:18 +0600

    get all products query added

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,5 +1,36 @@
+import { useGetAllProductsQuery } from '../redux/features/product/productApi'
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>
+  const { data, isLoading, isError } = useGetAllProductsQuery(undefined)
+  const products = data?.data
+
+  let content = null
+
+  if (isLoading) {
+    content = <h1 className='text-6xl font-bold text-green-800 '>Loading...</h1>
+  }
+
+  if (!isLoading && isError) {
+    content = <h1 className='text-6xl font-bold text-red-800 '>Error...</h1>
+  }
+
+  if (!isLoading && !isError && products?.length === 0) {
+    content = (
+      <h1 className='text-6xl font-bold text-red-800 '>No Data Found...</h1>
+    )
+  }
+
+  if (!isLoading && !isError && products?.length > 0) {
+    return (
+      <h1 className='text-2xl font-bold text-red-800 '>
+        {data?.data?.map((item) => {
+          return <li>{item?.name}</li>
+        })}
+      </h1>
+    )
+  }
+
+  return content
 }
 
 export default ProductsPage
---
+++ src/redux/features/product/productApi.tsx
@@ -0,0 +1,14 @@
+import { baseApi } from '../../api/baseApi'
+
+const productsApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllProducts: builder.query({
+      query: () => ({
+        url: '/products',
+        method: 'GET',
+      }),
+    }),
+  }),
+})
+
+export const { useGetAllProductsQuery } = productsApi
