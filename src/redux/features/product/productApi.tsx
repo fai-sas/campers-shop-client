d27295dabcd8ac3501f824998271d6dbcfdf90b6
fai-sas <craftedbyfaisal@gmail.com>
2024-07-09 20:10:26 +0600

    get single product query added

--- src/redux/features/product/productApi.tsx
+++ src/redux/features/product/productApi.tsx
@@ -8,6 +8,12 @@ const productsApi = baseApi.injectEndpoints({
         method: 'GET',
       }),
     }),
+    getSingleProduct: builder.query({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: 'GET',
+      }),
+    }),
   }),
 })
 
